fd599096726214deabfc89f7cf06fa8d0dce973a
Manshu Sharma <manshusharma@thehill.org>
2017-07-27 10:39:00 -0400

    MapClass

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,14 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Manshesht Sharma on 7/27/17.
+//  Copyright © 2017 The Hill School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
